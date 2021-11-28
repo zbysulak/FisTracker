ea3ef1c41ebe7916c68738d3326abbbe25fa9ebe
Martin Švec <martin.svec.ms@gmail.com>
2021-11-28 01:12:32 +0100

    [UPDATE] Home

--- fis-tracker-app/src/views/Home.vue
+++ fis-tracker-app/src/views/Home.vue
@@ -1,21 +1,11 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">Welcome to Fis Tracker</h1>
 
-        <p class="subheading font-weight-regular">
-          Track your time easily.
-        </p>
+        <p class="subheading font-weight-regular">Track your time easily.</p>
       </v-col>
       
     </v-row>
