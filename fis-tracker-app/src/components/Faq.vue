6161e3379a7d3910b68cadb8a9714d303d438381
Martin Švec <martin.svec.ms@gmail.com>
2021-12-21 17:58:25 +0100

    [ADD] FAQ skeleton

--- fis-tracker-app/src/components/Faq.vue
+++ fis-tracker-app/src/components/Faq.vue
@@ -1,7 +1,7 @@
 <template>
     <v-expansion-panels focusable>
         <v-expansion-panel>
-        <v-expansion-panel-header>faq 1</v-expansion-panel-header>
+        <v-expansion-panel-header>Jak má vypadat screenshot docházky pro nahrátí ?</v-expansion-panel-header>
         <v-expansion-panel-content>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </v-expansion-panel-content>
